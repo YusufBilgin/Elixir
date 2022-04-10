3eaaf0373da1909b877501f6ae3d4f3082c6e04a
yusuf b <ykbilgin61@gmail.com>
2022-04-10 06:45:05 +0300

    docs: add 'cond' notes

--- case_conf_and_if.exs
+++ case_conf_and_if.exs
@@ -33,3 +33,15 @@ f = fn
 end
 
 
+# cond syntax 
+
+cond do
+    2 + 2 == 5 ->
+        "This will not be True"
+    2 * 2 == 3 ->
+        "Nor this"
+    1 + 1 == 2 ->
+        "But this will"
+end    
+
+ 
