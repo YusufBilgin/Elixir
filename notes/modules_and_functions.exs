4b5bc2488db8cf2f31163dab97417ecf6837dcb9
yusuf b <ykbilgin61@gmail.com>
2022-04-16 23:36:30 +0300

    docs: edit 'function' notes

--- notes/modules_and_functions.exs
+++ notes/modules_and_functions.exs
@@ -18,3 +18,40 @@ end
 # _build - contains compilation artifacts
 # lib - contains Elixir code (usually .ex files)
 # test - contains tests (usually .exs files)
+
+
+# private functions (can only be invoked on local scope)
+defmodule Math do
+  def sum(a, b) do
+    do_sum(a, b)
+  end
+
+  defp do_sum(a, b) do
+    a + b
+  end
+end
+
+
+# Function declarations also support guards and multiple clauses
+defmodule Math do
+  def zero?(0) do
+    true
+  end
+
+  def zero?(x) when is_integer(x) do
+    false
+  end
+end
+
+
+# Named functions support also do: syntax
+defmodule Math do
+  def zero?(0), do: true
+  def zero?(x) when is_integer(x), do: false
+end
+
+
+# assign functions to variables
+Math.zero?(0)
+fun = &Math.zero?/1
+fun.(0)
